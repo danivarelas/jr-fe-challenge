ac1972130289e2b5e606a4ac3bceb537c2d09fa2
daniel varela serrano <danivarelas@hotmail.com>
2020-09-15 12:19:05 -0600

    changes for heroku deploy

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express();
+const port = 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", function (request, response){
+    //show this file when the "/" is requested
+    response.sendFile(__dirname+"/index.html");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
